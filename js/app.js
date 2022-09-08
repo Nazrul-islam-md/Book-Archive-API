acba486d729e26ef772b4da208e07ab34c120571
Nazrul <mohammadnazrulislam2k20@gmail.com>
2022-09-08 23:51:46 +0600

    added js code for showing book cover

--- js/app.js
+++ js/app.js
@@ -4,19 +4,25 @@ document.getElementById('button-search').addEventListener('click', function(){
     console.log(inputFieldText);
     inputField.value = '';
     const url = `https://openlibrary.org/search.json?q=${inputFieldText}`;
+    // display spinner
+    document.getElementById('spinner').style.display = 'block';
     fetch(url)
     .then(res => res.json())
     .then(data => displayBook(data.docs))
+    // .catch(error => console.log('Please insert a valid word'))
 })
 
 const displayBook = books => {
+    
     const cardHolder = document.getElementById('card-holder');
+    cardHolder.textContent = '';
     books.forEach(book => {
+        const imgUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="${imgUrl}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${book.title}</h5>
           <p class="card-text">Author Name: ${book.author_name}</p>
@@ -25,6 +31,9 @@ const displayBook = books => {
         </div>
       </div>
         `;
+        // undisplaying spinner
+        document.getElementById('spinner').style.display = 'none';
         cardHolder.appendChild(div);
+
     })
 }
