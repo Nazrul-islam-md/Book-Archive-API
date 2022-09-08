ac99bd02c5566790cd87d273bee5e8d3ca267f43
Nazrul <mohammadnazrulislam2k20@gmail.com>
2022-09-08 23:13:45 +0600

    added js code for book, author, publisher and publised year

--- js/app.js
+++ js/app.js
@@ -10,7 +10,21 @@ document.getElementById('button-search').addEventListener('click', function(){
 })
 
 const displayBook = books => {
+    const cardHolder = document.getElementById('card-holder');
     books.forEach(book => {
-        console.log(book.title);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${book.title}</h5>
+          <p class="card-text">Author Name: ${book.author_name}</p>
+          <p>Publisher: ${book.publisher[0]}</p>
+          <p>First Published: ${book.first_publish_year}</p>
+        </div>
+      </div>
+        `;
+        cardHolder.appendChild(div);
     })
 }
